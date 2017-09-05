4c9f2f72bf732fac0b3135eac7ea269710f76b3f
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-05 14:32:23 +0200

    One-to-Manny: Controllers

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,24 @@
 class Event < ApplicationRecord
-  belongs_to :user
+    belongs_to :user
 
-  validates :name, presence: true
-  validates :description, presence:true, length: { maximum: 500 }
+    validates :name, presence: true
+    validates :description, presence:true, length: { maximum: 500 }
+    validates :start_at, presence:true
+    validates :end_at, presence:true
+
+    validate :end_at_after_start_at?
+
+    change_column_default(:events, :price, 0)
+    change_column_default(:events, :includes_food, false)
+    change_column_default(:events, :includes_drinks, false)
+    change_column_default(:events, :active, true)
+
+
+    private
+
+    def end_at_after_start_at?
+      if end_at < start_at
+        errors.add :end_at, "must be after start date"
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
  root to: "pages#home"
   devise_for :users
 
+  resources :users, only: [:show]
+  resources :rooms, except: [:destroy]
+
 end
