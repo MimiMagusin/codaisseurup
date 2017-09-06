49687521053b2f0033197e13789dc001057afe5e
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-06 17:49:52 +0200

    Add Model Specs: Rooms

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,17 @@ class Event < ApplicationRecord
     belongs_to :user
     has_and_belongs_to_many :categories
 
-    validates :name, presence: true
+    validates :name, presence: true, length: { maximum: 50 }
     validates :description, presence:true, length: { maximum: 500 }
+
+    MAX_PRICE_BARGAIN = 30
+
+    def bargain?
+      price < MAX_PRICE_BARGAIN
+    end
+
+    def self.order_by_price
+      order :price
+    end
 
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:description)}
+    it { is_expected.to validate_length_of(:name).is_at_most(50) }
+    it { is_expected.to validate_length_of(:description).is_at_most(500) }
+  end
+
+  describe "bargain" do
+    let(:user)              { create :user }
+    let(:bargain_event)     { create :event, price: 20, user: user }
+    let(:no_bargain_event)  { create :event, price: 35, user: user }
+
+    it "returns true if the price is smaller than 30 EUR" do
+      expect(bargain_event.bargain?).to eq(true)
+      expect(no_bargain_event.bargain?).to eq(false)
+    end
+  end
+
+  describe "order_by_price" do
+    let(:user)    { create :user }
+    let!(:event1)  { create :event, price: 100, user: user }
+    let!(:event2)  { create :event, price: 20, user: user }
+    let!(:event3)  { create :event, price: 35, user: user }
+
+    it "returns an array of the events sorted by price" do
+      expect(Event.order_by_price).to eq([event2, event3, event1])
+    end
+  end
+
 end
