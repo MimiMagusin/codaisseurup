24390494bf50c0c39e87f23e83880c466835b6ef
MimiMagusin <mimi_magusin@hotmail.com>
2017-09-11 15:58:40 +0200

    Add API rooms destroy and update

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -30,6 +30,32 @@ class Api::EventsController < ApplicationController
     end
   end
 
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+
+    render status: 200, json: {
+      message: "event successfully deleted"
+    }.to_json
+  end
+
+  def update
+    event = Event.find(params[:id])
+    if event.update(event_params)
+      render status: 200, json: {
+        message: "event successfully updated",
+        event: event
+      }.to_json
+    else
+       render status: 422, json: {
+        message: "The event could not be updated",
+        errors: event.errors
+      }.to_json
+    end
+  end
+
+
+
   private
 
   def event_params
